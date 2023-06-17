035838560b401406e0a1508b25b9172fc434325a
mohamedalaa4j <mohamedalaa4j@gmail.com>
2023-06-17 19:27:58 +0300

    Custom Selection done

--- app/src/main/java/com/mohamedalaa4j/availabledatesevents/MainActivity.kt
+++ app/src/main/java/com/mohamedalaa4j/availabledatesevents/MainActivity.kt
@@ -1,11 +1,38 @@
 package com.mohamedalaa4j.availabledatesevents
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.applandeo.materialcalendarview.CalendarDay
+import com.applandeo.materialcalendarview.EventDay
+import com.applandeo.materialcalendarview.listeners.OnDayClickListener
+import com.mohamedalaa4j.availabledatesevents.databinding.ActivityMainBinding
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.calendarView.setSelectionBackground(R.drawable.custom_calender_background)
+
+        binding.calendarView.setOnDayClickListener(
+            object : OnDayClickListener {
+                override fun onDayClick(eventDay: EventDay) {
+                    val day = eventDay.calendar.get(Calendar.DAY_OF_MONTH)
+                    val month = eventDay.calendar.get(Calendar.MONTH) + 1
+                    val year = eventDay.calendar.get(Calendar.YEAR)
+//                    Toast.makeText(this@MainActivity, year.toString(), Toast.LENGTH_SHORT).show()
+                    Log.e("eventDay", year.toString())
+                }
+
+            }
+        )
     }
+
 }
